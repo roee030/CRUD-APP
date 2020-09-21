bfb7f1a928160e8fc2736965f693a430893a65d2
Roee Angel <roee.angel93@gmail.com>
2020-09-21 10:40:42 +0300

    CRUD React Task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Followers from "./Followers";
+import "./App.css";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Followers></Followers>
+      </div>
+    );
+  }
+}
---
+++ src/Followers.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Instagram extends Component {
+  state = {
+    followers: [],
+    name: "",
+    src: "",
+  };
+
+  componentDidMount = async () => {
+    const followers = await axios.get(
+      `https://5f636146363f0000162d8949.mockapi.io/ra/v1/followers`
+    );
+    this.setState({ followers: followers.data }, () => {});
+  };
+  handleOnClick = async () => {
+    const newAvatar = await axios.post(
+      `https://5f636146363f0000162d8949.mockapi.io/ra/v1/followers?name=${this.state.name}&src=${this.state.src}`
+    );
+    this.setState(
+      (prevState) => ({
+        followers: [...prevState.followers, newAvatar.data],
+      }),
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  render() {
+    const myFollowers = this.state.followers.map((element) => {
+      return (
+        <div className="followers" key={element.id}>
+          <img src={element.avatar}></img>
+          {element.name}
+        </div>
+      );
+    });
+    return (
+      <div>
+        <p className="followersTitle">My followers</p>
+        <form>
+          <label>
+            Name:
+            <input
+              type="text"
+              onChange={(e) => this.setState({ name: e.target.value })}
+            ></input>
+          </label>
+          <label>
+            SRC:
+            <input
+              type="text"
+              onChange={(e) => this.setState({ src: e.target.value })}
+            ></input>
+          </label>
+        </form>
+        <button onClick={this.handleOnClick}>Add follower</button>
+        {myFollowers}
+      </div>
+    );
+  }
+}
